4a698cc63a0f8cc6252f8c1aaee8509e69419a54
Irena773 <irina0773555666@gmail.com>
2020-11-26 02:27:54 +0900

    first commit for github
    first commit

---
+++ Assets/script/BallController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BallController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject Count;
+    //public GameObject ball;
+    public Text Score;
+    public int score = 0;
+    //private Vector2 BallStartPos;
+    void Start()
+    {
+       // BallStartPos = gameObject.transform.position;
+        Score = Count.GetComponent<Text>();
+        Score.text = "0";
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "piku")
+        {
+            score += 1;
+            Score.text = score.ToString();
+
+        }
+        else if (collision.gameObject.tag == "underground")
+        {
+            
+            score = 0;
+            Score.text = score.ToString();
+            // gameObject.transform.position = BallStartPos;
+            SceneManager.LoadScene("SampleScene");
+
+        }
+
+
+    }
+}
---
+++ Assets/script/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+
+    
+
+  
+    void Update()
+    {
+        
+        if (Input.GetKey(KeyCode.RightArrow)){
+
+            this.gameObject.transform.Translate(0.05f, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            this.gameObject.transform.Translate(-0.05f, 0, 0);
+        }
+    }
+
+    
+}
